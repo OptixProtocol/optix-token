631338c6960600dcd8b1ec0a4bcebd0183e505d3
dannydoritoeth <80568149+dannydoritoeth@users.noreply.github.com>
2024-04-24 16:54:29 +1000

    StakingContract updated

--- src/StakingRewards.sol
+++ src/StakingRewards.sol
@@ -1,116 +1,174 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {Test, console, console2} from "forge-std/Test.sol";
 
 /// @title Staking Rewards Contract
 /// @notice This contract allows users to stake tokens and earn rewards based on the staking duration and amount.
-contract StakingRewards is Ownable {
+contract StakingRewards is Ownable, ReentrancyGuard, Pausable {
+    using SafeERC20 for IERC20;
+
+    /* ========== STATE VARIABLES ========== */
+
     IERC20 public rewardsToken;
     IERC20 public stakingToken;
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public rewardsDuration = 7 days;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
 
-    uint public rewardRate = 1.7e18;                        // Initial reward rate
-    uint public lastUpdateTime;                             // Last time the reward calculation was updated
-    uint public rewardPerTokenStored;                       // Stored reward per token
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
 
-    mapping(address => uint) public userRewardPerTokenPaid; // Reward per token paid to each user
-    mapping(address => uint) public rewards;                // Reward amounts for each user
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
 
-    uint public totalSupply;                                // Total supply of staked tokens
-    mapping(address => uint) public balances;               // Balances of staked tokens for each user
+    /* ========== CONSTRUCTOR ========== */
 
-    //uint _blockTimestamp;
-
-    event RewardRateUpdated(uint from, uint to);
-    event Staked(address indexed user, uint256 amount);
-
-    // event RewardAdded(uint256 reward);
-    // event Withdrawn(address indexed user, uint256 amount);
-    // event RewardPaid(address indexed user, uint256 reward);
-    // event RewardsDurationUpdated(uint256 newDuration);
-    // event Recovered(address token, uint256 amount);
-
-    /// @param _stakingToken Address of the token being staked
-    /// @param _rewardsToken Address of the token being rewarded
-    constructor(address _stakingToken, address _rewardsToken) Ownable(msg.sender) {
-        stakingToken = IERC20(_stakingToken);
+    constructor(
+        address _rewardsToken,
+        address _stakingToken
+    ) Ownable(msg.sender) {
         rewardsToken = IERC20(_rewardsToken);
+        stakingToken = IERC20(_stakingToken);
     }
 
-    /// @notice Calculates the current reward per token
-    /// @return The calculated reward per token
-    function rewardPerToken() public view returns (uint) {
-        if (totalSupply == 0) {
+    /* ========== VIEWS ========== */
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return block.timestamp < periodFinish ? block.timestamp : periodFinish;
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {
             return rewardPerTokenStored;
         }
         return
-            rewardPerTokenStored +
-            (((blockTimestamp() - lastUpdateTime) * rewardRate * 1e18) / totalSupply);
+            rewardPerTokenStored + ((lastTimeRewardApplicable() - lastUpdateTime) * rewardRate * 1e18) / _totalSupply;
     }
 
-    /// @notice Calculates the amount of rewards earned by an account
-    /// @param account The account to calculate rewards for
-    /// @return The amount of rewards earned
-    function earned(address account) public view returns (uint) {
-        return
-            ((balances[account] *
-                (rewardPerToken() - userRewardPerTokenPaid[account])) / 1e18) +
-            rewards[account];
+    function earned(address account) public view returns (uint256) {
+        // console.log("rewards[account]: %s", rewards[account]);
+        // console.log("rewardPerToken(): %s", rewardPerToken());
+        // console.log("userRewardPerTokenPaid[account]: %s", userRewardPerTokenPaid[account]);
+        // console.log("_balances[account]: %s", _balances[account]);
+        return (_balances[account] * (rewardPerToken() - userRewardPerTokenPaid[account]) / 1e18) + rewards[account];
     }
 
-    /// @notice Updates the reward for an account upon staking, withdrawing, or claiming rewards
-    /// @param account The account to update the reward for
+    function getRewardForDuration() external view returns (uint256) {
+        return rewardRate*(rewardsDuration);
+    }
+
+    /* ========== MUTATIVE FUNCTIONS ========== */
+
+    function stake(uint256 amount) external nonReentrant whenNotPaused updateReward(msg.sender) {
+        require(amount > 0, "Cannot stake 0");
+        _totalSupply += amount;
+        _balances[msg.sender] = _balances[msg.sender]+amount;
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount) public nonReentrant updateReward(msg.sender) {
+        require(amount > 0, "Cannot withdraw 0");
+        _totalSupply += amount;
+        _balances[msg.sender] = _balances[msg.sender]-amount;
+        stakingToken.safeTransfer(msg.sender, amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function getReward() public nonReentrant updateReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function exit() external {
+        withdraw(_balances[msg.sender]);
+        getReward();
+    }
+
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+    }
+
+    /* ========== RESTRICTED FUNCTIONS ========== */
+
+    function notifyRewardAmount(uint256 reward) external onlyOwner updateReward(address(0)) {
+        if (block.timestamp >= periodFinish) {
+            rewardRate = reward/rewardsDuration;
+        } else {
+            uint256 remaining = periodFinish-block.timestamp;
+            uint256 leftover = remaining*rewardRate;
+            rewardRate = (reward+leftover)/rewardsDuration;
+        }
+
+        // Ensure the provided reward amount is not more than the balance in the contract.
+        // This keeps the reward rate in the right range, preventing overflows due to
+        // very high values of rewardRate in the earned and rewardsPerToken functions;
+        // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+        uint balance = rewardsToken.balanceOf(address(this));
+        require(rewardRate <= balance/rewardsDuration, "Provided reward too high");
+
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp+(rewardsDuration);
+        emit RewardAdded(reward);
+    }
+
+    // // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
+    // function recoverERC20(address tokenAddress, uint256 tokenAmount) external onlyOwner {
+    //     require(tokenAddress != address(stakingToken), "Cannot withdraw the staking token");
+    //     IERC20(tokenAddress).safeTransfer(owner, tokenAmount);
+    //     emit Recovered(tokenAddress, tokenAmount);
+    // }
+
+    function setRewardsDuration(uint256 _rewardsDuration) external onlyOwner {
+        require(
+            block.timestamp > periodFinish,
+            "Previous rewards period must be complete before changing the duration for the new period"
+        );
+        rewardsDuration = _rewardsDuration;
+        emit RewardsDurationUpdated(rewardsDuration);
+    }
+
+    /* ========== MODIFIERS ========== */
+
     modifier updateReward(address account) {
         rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = blockTimestamp();
-
-        rewards[account] = earned(account);
-        userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
         _;
     }
 
-    /// @notice Stakes a certain amount of tokens
-    /// @param _amount The amount of tokens to stake
-    function stake(uint _amount) external updateReward(msg.sender) {
-        require(_amount > 0, "Cannot stake 0");
-        totalSupply += _amount;
-        balances[msg.sender] += _amount;
-        stakingToken.transferFrom(msg.sender, address(this), _amount);
-    }
+    /* ========== EVENTS ========== */
 
-    /// @notice Withdraws staked tokens
-    /// @param _amount The amount of tokens to withdraw
-    function withdraw(uint _amount) external updateReward(msg.sender) {
-        totalSupply -= _amount;
-        balances[msg.sender] -= _amount;
-        stakingToken.transfer(msg.sender, _amount);
-    }
-
-    /// @notice Claims the earned rewards
-    function getReward() external updateReward(msg.sender) {
-        uint reward = rewards[msg.sender];
-        rewards[msg.sender] = 0;
-        rewardsToken.transfer(msg.sender, reward);
-    }
-
-    /// @notice Sets a new reward rate
-    /// @param _rewardRate The new reward rate to set
-    function setRewardRate(uint256 _rewardRate) public onlyOwner{
-        emit RewardRateUpdated(rewardRate, _rewardRate);
-        rewardRate = _rewardRate;
-    }
-
-    /// @notice Gets the current block timestamp
-    /// @return The current block timestamp
-    function blockTimestamp() public view returns (uint) {
-        return block.timestamp;
-        // return _blockTimestamp;
-    }
-
-    // function setBlockTimestamp(uint256 _newBlockTimestamp) public {
-    //     _blockTimestamp = _newBlockTimestamp;
-    // }
-    
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+    event RewardsDurationUpdated(uint256 newDuration);
+    event Recovered(address token, uint256 amount);
 }
-
--- test/StakingRewards.t.sol
+++ test/StakingRewards.t.sol
@@ -4,101 +4,99 @@ pragma solidity ^0.8.20;
 import {Test} from "forge-std/Test.sol";
 import {StakingRewards} from "src/StakingRewards.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
-
+import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract StakingRewardsTest is Test {
     StakingRewards public stakingRewards;
     MockERC20 public stakingToken;
     MockERC20 public rewardsToken;
 
-    address public owner;
-    address public user1;
+    address private owner;
+    address private user1;
+    address private user2;
 
     function setUp() public {
-        owner = address(this);
-        user1 = address(0x1);
+            owner = address(this); // Set the contract deployer as owner
+            user1 = vm.addr(1);
+            user2 = vm.addr(2);
 
-        // Deploy mock tokens
-        stakingToken = new MockERC20("Staking Token", "STK", 18);
-        rewardsToken = new MockERC20("Rewards Token", "RWD", 18);
+            rewardsToken = new MockERC20("Rewards Token", "RWT", 18);
+            stakingToken = new MockERC20("Staking Token", "STK", 18);
 
-        // Allocate some tokens for testing
-        stakingToken.mint(owner, 1e24); // 1 million tokens
-        stakingToken.mint(user1, 1e24);
-        rewardsToken.mint(address(this), 1e24);
+            rewardsToken.mint(owner, 1_000_000e18); // Mint some tokens for the owner
+            stakingToken.mint(user1, 100_000e18);   // Mint staking tokens for user1
+            stakingToken.mint(user2, 100_000e18);   // Mint staking tokens for user2
 
-        // Deploy the StakingRewards contract
-        stakingRewards = new StakingRewards(address(stakingToken), address(rewardsToken));
+            vm.prank(owner); 
+            stakingRewards = new StakingRewards(
+                address(rewardsToken),
+                address(stakingToken)
+            );
 
-        // Approve StakingRewards contract to spend on behalf of owner and user1
-        stakingToken.approve(address(stakingRewards), type(uint256).max);
-        vm.prank(user1); stakingToken.approve(address(stakingRewards), type(uint256).max);
-    }
+            // Approve staking token to stakingRewards from users
+            vm.prank(user1); stakingToken.approve(address(stakingRewards), 100_000e18);
+            vm.prank(user2); stakingToken.approve(address(stakingRewards), 100_000e18);
+        }
 
-    function test_StakeTokens() public {
-        uint256 amountToStake = 1e23; // 100k tokens
-        stakingRewards.stake(amountToStake);
+        function test_SR_PauseAndUnpauseStaking() public {
+            vm.prank(owner); stakingRewards.pause();
+            assertTrue(stakingRewards.paused(), "Staking should be paused");
 
-        // Verify the state after staking
-        assertEq(stakingToken.balanceOf(address(stakingRewards)), amountToStake, "StakingRewards contract did not receive the tokens");
-        assertEq(stakingRewards.balances(owner), amountToStake, "Owner's staked balance is incorrect");
-        assertEq(stakingRewards.totalSupply(), amountToStake, "Total supply is incorrect");
-    }
+            vm.prank(owner); vm.expectRevert(Pausable.EnforcedPause.selector);
+            stakingRewards.stake(10_000e18);
 
-    function testFuzz_StakeTokens(uint256 amountToStake) public {
-        // Skip overflows and 0 staking
-        vm.assume(amountToStake > 0 && amountToStake <= 1e24);
+            vm.prank(owner); stakingRewards.unpause();
+            assertFalse(stakingRewards.paused(), "Staking should be unpaused");
 
-        stakingRewards.stake(amountToStake);
+            vm.prank(user1); stakingRewards.stake(10_000e18); // Should succeed after unpausing
+        }
 
-        // Verify the state after staking
-        assertEq(stakingToken.balanceOf(address(stakingRewards)), amountToStake, "StakingRewards contract did not receive the tokens");
-        assertEq(stakingRewards.balances(owner), amountToStake, "Owner's staked balance is incorrect");
-        assertEq(stakingRewards.totalSupply(), amountToStake, "Total supply is incorrect");
-    }
+        function test_SR_RewardDistribution() public {
+            uint amount = 10_000e18;
+            uint rewardAmount = 500e18;
 
-    function test_WithdrawTokens() public {
-        uint256 amountToStake = 1e23; // 100k tokens
-        stakingRewards.stake(amountToStake);
-        stakingRewards.withdraw(amountToStake);
+            vm.prank(user1); stakingRewards.stake(amount);
 
-        // Verify the state after withdrawing
-        assertEq(stakingToken.balanceOf(address(this)), 1e24, "Owner did not receive the tokens back");
-        assertEq(stakingRewards.balances(owner), 0, "Owner's staked balance should be zero");
-        assertEq(stakingRewards.totalSupply(), 0, "Total supply should be zero");
-    }
+            vm.startPrank(owner);
+            rewardsToken.transfer(address(stakingRewards), rewardAmount);
+            stakingRewards.notifyRewardAmount(rewardAmount);
+            vm.stopPrank();
 
-    function test_UpdateRewardRate() public {
-        // Setup: Initial reward rate
-        uint256 initialRate = stakingRewards.rewardRate();
-        uint256 newRate = initialRate + 1e18; // Increase by 1 token per second
+            vm.warp(block.timestamp + 5 days); // Fast-forward time
 
-        // Act: Update reward rate
-        stakingRewards.setRewardRate(newRate);
+            vm.startPrank(user1);
+            uint earnedBefore = stakingRewards.earned(user1);
+            stakingRewards.getReward(); // Claim reward
+            uint earnedAfter = stakingRewards.earned(user1);
+            assertLt(earnedAfter, earnedBefore, "Earned rewards should decrease after claiming");
+            uint balanceAfter = rewardsToken.balanceOf(user1);
+            assertTrue(balanceAfter > 0, "User should have received rewards tokens");
+            vm.stopPrank();
+        }
 
-        // Assert: Reward rate is updated
-        assertEq(stakingRewards.rewardRate(), newRate, "Reward rate not updated correctly");
+        function test_SR_OnlyOwnerCanPauseAndUnpause() public {
+            vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user1));
+            vm.prank(user1); // user1 is not the owner
+            stakingRewards.pause();
 
-        // Assert: Only owner can update
+            vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user2));
+            vm.prank(user2); // user2 is not the owner
+            stakingRewards.unpause();
+        }
 
-        vm.expectRevert("Ownable: caller is not the owner");
-        vm.prank(user1); stakingRewards.setRewardRate(initialRate);
-    }
+        function test_SR_OnlyOwnerCanNotifyRewardAmount() public {
+            uint256 rewardAmount = 1e18;
+            vm.prank(owner); rewardsToken.transfer(address(stakingRewards), rewardAmount);
 
+            // Only owner should be able to notify reward amount
+            vm.expectRevert(abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, user1));
+            vm.prank(user1);
+            stakingRewards.notifyRewardAmount(rewardAmount);
 
-    function testFuzz_WithdrawTokens(uint256 amountToStake, uint256 amountToWithdraw) public {
-        vm.assume(amountToStake > 0 && amountToStake <= 1e24 && amountToWithdraw <= amountToStake);
-        stakingRewards.stake(amountToStake);
-
-        stakingRewards.withdraw(amountToWithdraw);
-
-    }
-
-
-    function testFuzz_ClaimRewards(uint256 amountToStake, uint256 timeToWait) public {
-        vm.assume(amountToStake > 0 && amountToStake <= 1e24 && timeToWait < 365 days);
-        stakingRewards.stake(amountToStake);
-        vm.warp(block.timestamp + timeToWait);
-    }   
+            // Successful call by owner
+            vm.prank(owner);
+            stakingRewards.notifyRewardAmount(rewardAmount);
+        }
 
 }
